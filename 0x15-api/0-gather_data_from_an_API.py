32a10606ba0cb698ece22b4bf0f7d7f14c6c6fff
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-03-25 09:10:13 -0700

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""returns information about employee"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    Id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+    url = url + "/" + Id
+    response = requests.get(url)
+    name = response.json().get('name')
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    count = 0
+    task_list = []
+    for task in tasks:
+        if task.get('completed'):
+            task_list.append(task)
+            count += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, count, len(tasks)))
+    for task in task_list:
+        print("\t {}".format(task.get('title')))
