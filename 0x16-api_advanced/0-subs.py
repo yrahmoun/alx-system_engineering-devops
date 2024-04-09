b704f5c4db64cb4bc66bb16e2ee84d7636101e0e
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-04-09 03:00:33 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,9 @@ def number_of_subscribers(subreddit):
         return 0
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=user_agent)
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
     results = response.json()
-    subs = results.get('data').get('subscribers')
+    subs = results.get("data").get("subscribers")
     return subs
